a6a10495778943fcf31be99a07b8180b731e555c
Your Name <you@example.com>
2026-04-01 04:55:55 -0700

    Axios,UseEffects

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,37 +2,23 @@ import React from 'react'
 
 const Cards = () => {
 
-     const cardsdata = [
-        {
-            id : 1,
-            title : "Card 1",
-            description : "This is the description for Card 1",
-            image_url : "./product-1.jpg"
-        },
-        {
-            id : 2,
-            title : "Card 2",
-            description : "This is the description for Card 2",
-            image_url : "./product-2.jpg"
-        },
-        {
-            id : 3,
-            title : "Card 3",
-            description : "This is the description for Card 3",
-            image_url : "./product-3.jpg"
-        }
-     ]
-
+    async function FetchProducts() {
+        
+    }
+     
 
   return (
 
      <>
-        <div id='parent'>
+        <div className='gird gird-cols-3 gap-20 py-24 px-10'>
             <div className="card">
                 <div className="card-banner">
                     <img src="" alt="" />
                 </div>
-                <div className="card-details"></div>
+                <div className="card-details">
+                    <p></p>
+                    <p></p>
+                </div>
             </div>
          </div>
      </>
---
+++ src/pages/UseEffects.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react' 
+
+const UseEffects = () => {
+  
+    const [counter,setCounter] = useState(0)
+    const [toggle,setToggle]   = useState(false)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+  
+  
+  
+  
+    return (
+    <>
+      
+    </>
+  )
+}
+
+export default UseEffects
